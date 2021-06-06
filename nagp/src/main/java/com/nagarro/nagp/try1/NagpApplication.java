fa5298d9b9c270bb9828a68c8a3c8a6dddbc236f
hiteshsatija1997 <hitsat29@gmail.com>
2021-06-06 20:45:19 +0530

    tesyt

---
+++ nagp/src/main/java/com/nagarro/nagp/try1/NagpApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.try1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagpApplication.class, args);
+	}
+
+}
---
+++ nagp/src/test/java/com/nagarro/nagp/try1/NagpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.try1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
